ffc8fabee23ea804dac7cd49809df6ae8f834cad
gabriel barcelos <gabrielvieirabarcelos02@gmail.com>
2021-02-11 22:50:50 -0300

    styled-components

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import {Title,InputText} from './src/style.js'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <Title></Title>
+    <InputText></InputText>
+    <Title onPress={()=>console.log("teste")}> teste</Title>
+    <WebView source={{ uri: 'https://www.cassol.com.br/' }}
+    
+     />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Title = styled.Text`
+  margin-top:10px;
+`
+export const InputText = styled.TextInput`
+  text-align:center;
+  background-color:red;
+`
